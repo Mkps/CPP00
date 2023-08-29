fff9f166d341bb5e101034f2bdab3ef619fa881e
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-08-29 22:22:20 +0200

    added constructor/destructor

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,1 +1,15 @@
 #include "Account.hpp"
+
+Account::Account( int initial_deposit )
+{
+	this->_amount = initial_deposit;
+}
+
+Account::~Account( void )
+{
+}
+static int	getNbAccounts( void );
+static int	getTotalAmount( void );
+static int	getNbDeposits( void );
+static int	getNbWithdrawals( void );
+static void	displayAccountsInfos( void );
