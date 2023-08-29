f81c09246dd203baa9847b1d608e0792a206c8be
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-08-30 01:41:45 +0200

    Fix on displayAccountStatus

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -48,6 +48,7 @@ int	Account::getNbWithdrawals( void )
 
 void	Account::displayAccountsInfos(void)
 {
+	Account::_displayTimestamp();
 	std::cout << "accounts:" << Account::getNbAccounts() << ";";
 	std::cout << "total:" << Account::getTotalAmount() << ";";
 	std::cout << "deposits:" << Account::getNbDeposits() << ";";
@@ -91,8 +92,8 @@ void	Account::displayStatus( void ) const
 	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";";
 	std::cout << "amount:" << this->_amount << ";";
-	std::cout << "nb_deposits:" << this->_nbDeposits << ";";
-	std::cout << "nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+	std::cout << "deposits:" << this->_nbDeposits << ";";
+	std::cout << "withdrawals:" << this->_nbWithdrawals << std::endl;
 }
 static std::string	DateString(void)
 {
