a82b82260b3fc44ce2e56f1ff22c32a7d9dd543f
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-08-30 02:04:44 +0200

    Added simple shell script to test that the outputs match.

---
+++ ex02/test_Account.sh
@@ -0,0 +1,28 @@
+RED="\033[31m"
+GREEN="\033[32m"
+NC="\033[0m"
+c++ -Wno-deprecated Account.cpp tests.cpp > /dev/null
+exit_code=$?
+if [ $exit_code -eq 0 ]
+then
+	result="OK"
+	color=$GREEN
+else
+	result="KO"
+	color=$RED
+fi
+printf "The program compiles:	${color}[%s]\n${NC}" $result
+./a.out > tmp_I
+cat -e tmp_I | cut -c 19- > cut_I
+cat -e 19920104_091532.log | cut -c 19- > cut_O
+diff cut_I cut_O
+exit_code=$?
+if [ $exit_code -eq 0 ]
+then
+	result="OK"
+	color=$GREEN
+else
+	result="KO"
+	color=$RED
+fi
+printf "Output matches previous log when timestamp is truncated:	${color}[%s]\n${NC}" $result
